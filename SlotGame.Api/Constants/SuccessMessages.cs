d6010b2f369b567bf3ca2cb23ebb259791dea792
simeon-chakarov <simeon.tchakarov@gmail.com>
2026-03-17 17:42:34 +0200

    CleanUp

--- SlotGame.Api/Constants/SuccessMessages.cs
+++ SlotGame.Api/Constants/SuccessMessages.cs
@@ -1,6 +1,6 @@
 ﻿namespace SlotGame.Api.Constants;
 
-public class SuccessMessages
+public static class SuccessMessages
 {
     public const string ApiIsWorking = "API is working";
     public const string DatabaseCleared = "Database cleared.";
